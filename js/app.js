33932eef12573fdf7d8fb2e83c99e8e28e26a7aa
alexL <aleksandrlambert@gmail.com>
2024-03-25 16:54:41 +0700

    добавляет фетч запрос на гитАпи через асинхронную функцию

--- js/app.js
+++ js/app.js
@@ -12,18 +12,19 @@ const mainEl = document.querySelector(".main");
 
 const formEl = document.createElement("form");
 formEl.classList.add('search');
-formEl.addEventListener('submit', (e) => {
+formEl.addEventListener('submit', async (e) => {
     e.preventDefault();
 
 //логика фетч-запроса
 const inputsValue = Object.fromEntries(new FormData(e.target));
+const response = await fetch(`https://api.github.com/users/${inputsValue.name}`);
 
 })
 
 
 const inputEl = document.createElement("input");
 inputEl.classList.add('.search-input');
-// inputEl.setAttribute('name', 'name');
+inputEl.setAttribute('name', 'name');
 
 const searchButtonEl = document.createElement("button");
 searchButtonEl.classList.add('search-button');
