f11cbe77f2ea57e13418adefbb7f5fe30e82ba2e
alexL <aleksandrlambert@gmail.com>
2024-03-25 17:08:40 +0700

    функция отображения спарсенных данных

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ const response = await fetch(`https://api.github.com/users/${inputsValue.name}`)
 if (response.ok) {
 const data = await response.json();
 // Использовать функцию для отрисовки данных
+// Добавить результат выше исполненной функции в mainEl
 } else {
     alert("Пользователь не найден");
 }
@@ -38,7 +39,21 @@ searchButtonEl.classList.add('search-button');
 searchButtonEl.setAttribute('type', 'submit');
 searchButtonEl.innerHTML = "Поиск";
 
-
 formEl.appendChild(inputEl);
 formEl.appendChild(searchButtonEl);
 mainEl.appendChild(formEl);
+
+function createProfileEl(profileData) {
+    const element = document.querySelector('div');
+    element.classList.add('profile');
+    element.innerHTML = `
+    <img class="search-image" src=${profileData.avatar_url}></img>
+    <p class="search-text"><span>Имя: </span>${profileData.name}</p>
+    <p class="search-text"><span>Город: </span>${profileData.location}</p>
+    <p class="search-text"><span>О себе: </span>${profileData.bio}</p>
+    `;
+}
+
+function createDeleteBtnEl() {
+
+}
