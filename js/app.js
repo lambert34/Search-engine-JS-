c1469cfd213e47a6377085086516c085ae62ee53
alexL <aleksandrlambert@gmail.com>
2024-03-25 15:03:21 +0700

    выполнили первые 6 шагов
    написали необходимые элементы и добавили в них необходимые компоненты

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const mainEl = document.querySelector(".main");
+
+// написать форму 
+// написать input
+// написать кнопку search
+
+// добавить input внутрь формы
+// добавить button внутрь формы
+// добавить форму к main
+
+// добавить ко всем этим 
+
+const formEl = document.createElement("form");
+const inputEl = document.createElement("input");
+const searchButtonEl = document.createElement("button");
+
+formEl.appendChild(inputEl);
+formEl.appendChild(searchButtonEl);
+mainEl.appendChild(formEl);
